86502276bb674fd1e119819150af9f209208f25c
wasim <wasimysdev@gmail.com>
2024-05-13 19:29:50 +0200

    Add Toaster component and integrate in layout
    A new component, Toaster, is added to handle toast notifications in the application. This component is integrated into the layout and is designed to render toast notifications based on the toast settings provided. Adding this provides a standard way to handle and display notifications within the application.

---
+++ src/lib/Toaster.svelte
@@ -0,0 +1,18 @@
+<script>
+	let { toast: toastSnippet } = $props();
+	
+	let toasts = $state([]);
+	
+	export function toast(settings) {
+		const id = Math.random();
+		settings.id = id;
+		toasts.push(settings);
+		setTimeout(() => {
+			toasts = toasts.filter(toast => toast.id !== id);
+		}, 2500);
+	}
+</script>
+
+{#each toasts as toast}
+	{@render toastSnippet(toast)}
+{/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,9 @@
 	import { pocketbase, userRune } from '$lib/pocketbase/index.svelte';
 	import { LocalStorage } from '$lib/utils/localStorage.svelte';
 	import type { AuthModel } from 'pocketbase';
-
+	import Toaster from '$lib/Toaster.svelte';
+	
+	let toaster = $state();
 	
 	let { children } = $props();
 	let layout = $state(0);
@@ -26,6 +28,7 @@
 
 <!-- <TitleBar /> -->
 {#if userRune.authStore}
+	
 	<div
 		class="hidden  bg-gray-400/10  backdrop-blur-2xl lg:fixed lg:inset-y-0 lg:left-0 lg:z-50 lg:block lg:w-16 lg:overflow-y-auto  lg:pb-4">
 		<nav class=" h-full overflow-y-hidden">
@@ -116,7 +119,7 @@
 					</svg>
 				</button>
 			</div>
-			<li class=" absolute bottom-0 w-full  mt-auto overflow-x-hidden">
+			<div class=" absolute bottom-0 w-full  mt-auto overflow-x-hidden">
 				<div
 					class="flex items-center  py-3 text-sm justify-between font-semibold leading-6 text-slate-200 hover:bg-slate-400/35">
 					<div class="flex gap-4 px-6 items-center">
@@ -136,7 +139,7 @@
 						</svg>
 					</button>
 				</div>
-			</li>
+			</div>
 			{#if layout === 0}
 				<!--todo message feed clear -->
 				
@@ -161,8 +164,57 @@
 	
 	</aside>
 {:else}
-	<Authentication />
+	<!--<Authentication />-->
+	<Toaster bind:this={toaster}>
+		{#snippet toast(settings)}
+		<div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
+			<div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+			
+				<div class="pointer-events-auto {settings.active ===true ? 'toastEnter' : 'toastLeave'} w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+					<div class="p-4">
+						<div class="flex items-start">
+							<div class="flex-shrink-0">
+								<svg class="h-6 w-6 text-green-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+								</svg>
+							</div>
+							<div class="ml-3 w-0 flex-1 pt-0.5">
+								<p class="text-sm font-medium text-gray-900">{settings.title}</p>
+								<p class="mt-1 text-sm text-gray-500">{settings.description}</p>
+							</div>
+							<div class="ml-4 flex flex-shrink-0">
+								<button type="button" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+									<span class="sr-only">Close</span>
+									<svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+										<path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+									</svg>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		{/snippet}
+	</Toaster>
+	<button onclick={() => toaster?.toast({
+	title: "something went right",
+	description: "nice",
+	active: true,
+	
+	 })}>Toast!</button>
+
 {/if}
 
 
+<style lang="postcss">
+    .toastEnter {
+        @apply transform ease-out duration-300 transition translate-y-0 opacity-100 sm:translate-x-0;
+    }
 
+    .toastLeave {
+        @apply transition ease-in duration-100 opacity-0;
+    }
+
+</style>
