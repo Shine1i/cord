cd8b31f11040e0d6e40a6133b29a1433b9d259ff
chayc <svynxc@gmail.com>
2024-05-16 01:03:40 -0400

    super cool

--- src/lib/Chat.ts
+++ src/lib/Chat.ts
@@ -3,20 +3,58 @@ import { pocketbase, userRune } from '$lib/pocketbase/index.svelte';
 class Chat {
 	allChats: any = [];
 	allMessages: any = [];
-	
+
 	async getChatWithUsers() {
 		try {
-			return await pocketbase.collection('chats').getFullList(200, {
+
+			//todo:get last message etc
+			const chats = await pocketbase.collection('chats').getFullList(200, {
 				sort: 'created',
-				expand: 'users',
-				relation: `users ~ '${userRune.authStore.id}'`
+				expand: `relation`,
+
 			});
+			const topThree = chats.slice(0, 3);
+			const lastMessagse = await Promise.all(topThree.map(async (chat) => {
+				const message = await pocketbase.collection('messages').getFirstListItem(`chat_id = '${chat.id}'`,{
+					sort: 'created',
+				});
+				
+				return message;
+			}));
+
+			
+			const typedChats: {
+				name: string,
+				avatar: string,
+				lastMessage: string|undefined,
+				messageTimestamp: string|undefined
+			}[] = [];
+			
+			for (let i = 0; i < chats.length; i++) {
+				const chat = chats[i];
+				const relation = chat.expand?.relation as any[];
+				for (let j = 0; j < relation.length; j++) {
+					const user = relation[j];
+					if (user.id !== userRune.authStore.id) {
+						typedChats.push({
+							name: user.username,
+							avatar: user.avatar,
+							lastMessage: lastMessagse[i].content,
+							messageTimestamp: lastMessagse[i].created
+						});
+					}
+				}
+
+			}
+
+			return typedChats;
+
 		} catch (e) {
 			console.log(e);
 			return [];
 		}
 	}
-	
+
 	async createChatWithUser(username: string) {
 		try {
 			const otherUser = await pocketbase.collection('users').getFirstListItem(`username = '${username}'`);
@@ -27,7 +65,7 @@ class Chat {
 			console.log(e);
 		}
 	}
-	
+
 	async getAllMessages(chatId: string) {
 		try {
 			return await pocketbase.collection('messages').getFullList(200, {
@@ -39,7 +77,7 @@ class Chat {
 			return [];
 		}
 	}
-	
+
 	async sendMessage(chatId: string) {
 		try {
 			await pocketbase.collection('messages').create({
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './styles.css';
 	import TitleBar from '$lib/components/TitleBar.svelte';
+	import { chat } from '$lib/Chat';
 	import MessageFeed from '$lib/components/chat_components/MessageFeed.svelte';
 	import { goto } from '$app/navigation';
 	import Friend from '$lib/components/Friend.svelte';
@@ -12,97 +13,123 @@
 	import toast from '$lib/Toaster.svelte';
 	import { fly } from 'svelte/transition';
 	import { flip } from 'svelte/animate';
-	
+
 	let toaster = $state();
-	
+
 	let { children } = $props();
 	let layout = $state(0);
-	const userLocalStorage = new LocalStorage<AuthModel>('user', null)
+	const userLocalStorage = new LocalStorage<AuthModel>('user', null);
 
-	$effect(()=>{
-		
+	$effect(() => {
 		if (userLocalStorage.value !== null && userLocalStorage.key !== '') {
 			userRune.authStore = userLocalStorage.value;
 		}
 		pocketbase.authStore.onChange(async (token, model) => {
 			userLocalStorage.value = model;
 		});
-	})
+		chat.getChatWithUsers().then(x=>{console.log(x);
+		})
+	});
+
 </script>
 
 <!-- <TitleBar /> -->
 {#if userRune.authStore}
-	
 	<div
-		class="hidden  bg-gray-400/10  backdrop-blur-2xl lg:fixed lg:inset-y-0 lg:left-0 lg:z-50 lg:block lg:w-16 lg:overflow-y-auto  lg:pb-4">
+		class="hidden bg-gray-400/10 backdrop-blur-2xl lg:fixed lg:inset-y-0 lg:left-0 lg:z-50 lg:block lg:w-16 lg:overflow-y-auto lg:pb-4"
+	>
 		<nav class=" h-full overflow-y-hidden">
-			<ul role="list"
-					class=" pt-4 flex relative min-h-full flex-col items-center space-y-1">
+			<ul role="list" class=" pt-4 flex relative min-h-full flex-col items-center space-y-1">
 				<li>
 					<button
-						onclick={()=>{
-							goto("/calls");
+						onclick={() => {
+							goto('/calls');
 							layout = 0;
 						}}
-						class=" {layout === 0 ? 'bg-slate-400/30' : 'bg-none'} text-slate-200 group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold">
-						<svg class="h-6 w-6 shrink-0" xmlns="http://www.w3.org/2000/svg"
-								 viewBox="0 0 24 24" fill="currentColor">
+						class=" {layout === 0
+							? 'bg-slate-400/30'
+							: 'bg-none'} text-slate-200 group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold"
+					>
+						<svg
+							class="h-6 w-6 shrink-0"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+						>
 							<path
-								d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+								d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z"
+							/>
 							<path
-								d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+								d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z"
+							/>
 						</svg>
 					</button>
 				</li>
 				<li>
 					<button
-						onclick={()=>{
-							goto("/friends");
+						onclick={() => {
+							goto('/friends');
 							layout = 1;
 						}}
-						class=" text-slate-200 {layout === 1 ? 'bg-slate-400/30' : 'bg-none'} hover:bg-slate-400/30  group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-								 fill="currentColor" class="w-6 h-6">
-							<path fill-rule="evenodd"
-										d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-										clip-rule="evenodd" />
+						class=" text-slate-200 {layout === 1
+							? 'bg-slate-400/30'
+							: 'bg-none'} hover:bg-slate-400/30 group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+								clip-rule="evenodd"
+							/>
 						</svg>
 					</button>
 				</li>
 				<li class="absolute bottom-0">
 					<button
-						onclick={()=>{
-							goto("#");
+						onclick={() => {
+							goto('#');
 							layout = 2;
 						}}
-						class=" text-slate-200 {layout === 2 ? 'bg-slate-400/30' : 'bg-none'} hover:bg-slate-400/30 group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-								 fill="currentColor" class="w-6 h-6">
-							<path fill-rule="evenodd"
-										d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
-										clip-rule="evenodd" />
+						class=" text-slate-200 {layout === 2
+							? 'bg-slate-400/30'
+							: 'bg-none'} hover:bg-slate-400/30 group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+								clip-rule="evenodd"
+							/>
 						</svg>
 					</button>
 				</li>
-			
 			</ul>
 		</nav>
 	</div>
-	
-	
-	<main id="main" class="lg:pl-20 ">
-		<div class="xl:pl-80 ">
-			<div class="px-4 pt-4  bg-[#343434] h-screen">
+
+	<main id="main" class="lg:pl-20">
+		<div class="xl:pl-80">
+			<div class="px-4 pt-4 bg-[#343434] h-screen">
 				{@render children()}
 			</div>
 		</div>
 	</main>
-	
+
 	<aside
-		class="fixed inset-y-0 overflow-x-hidden  left-12 hidden w-[352px] bg-gray-400/10 shadow-md shadow-slate-400  backdrop-blur-2xl overflow-y-auto    py-3 xl:block">
-		<div class="flex flex-col px-4 ">
-			<div
-				class="flex flex-wrap  py-4 items-center justify-between sm:flex-nowrap">
+		class="fixed inset-y-0 overflow-x-hidden left-12 hidden w-[352px] bg-gray-400/10 shadow-md shadow-slate-400 backdrop-blur-2xl overflow-y-auto py-3 xl:block"
+	>
+		<div class="flex flex-col px-4">
+			<div class="flex flex-wrap py-4 items-center justify-between sm:flex-nowrap">
 				<div class="">
 					<h3 class="text-base pl-3 font-semibold leading-6 text-white">
 						{#if layout === 0}
@@ -112,49 +139,70 @@
 						{/if}
 					</h3>
 				</div>
-				
-				<button
-					class="flex-shrink-0 text-slate-200 {layout === 0 ? 'block' : 'hidden'} ">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none"
-							 viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-							 class="w-6 h-6 ">
-						<path stroke-linecap="round" stroke-linejoin="round"
-									d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+
+				<button class="flex-shrink-0 text-slate-200 {layout === 0 ? 'block' : 'hidden'} ">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+						/>
 					</svg>
 				</button>
 			</div>
-			<div class=" absolute bottom-0 w-full  mt-auto overflow-x-hidden">
+			<div class=" absolute bottom-0 w-full mt-auto overflow-x-hidden">
 				<div
-					class="flex items-center  py-3 text-sm justify-between font-semibold leading-6 text-slate-200 hover:bg-slate-400/35">
+					class="flex items-center py-3 text-sm justify-between font-semibold leading-6 text-slate-200 hover:bg-slate-400/35"
+				>
 					<div class="flex gap-4 px-6 items-center">
 						<img
 							class="h-8 w-8 rounded-md bg-red-500 ring-2 border border-red-300 ring-red-300"
 							src={`https://api.dicebear.com/8.x/initials/svg?seed=${userRune.authStore.username}&backgroundColor=ff6666`}
-							alt="">
-						<span
-							aria-hidden="true">{userRune.authStore.username.toUpperCase()}</span>
+							alt=""
+						/>
+						<span aria-hidden="true">{userRune.authStore.username.toUpperCase()}</span>
 					</div>
 					<button>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none"
-								 viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-								 class="w-6 h-6 mr-6">
-							<path stroke-linecap="round" stroke-linejoin="round"
-										d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6 mr-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+							/>
 						</svg>
 					</button>
 				</div>
 			</div>
 			{#if layout === 0}
+				<!-- load from db -->
 				<!--todo message feed clear -->
+				{#await chat.getChatWithUsers()}
+					<p>Loading...</p>
+				{:then users}
+					{#each users as user}
+						<MessageFeed
+							avatar="{user.avatar}"
+							name={user.name}
+							lastMessage={user.lastMessage??''}
+							messageTimeStamp={user.messageTimestamp?new Date(user.messageTimestamp).toLocaleDateString():''}
+						/>
+					{/each}
+				{/await}
 				
-				{#each Array(5) as chat}
-					<MessageFeed
-						avatar=""
-						name={'placehlder'}
-						lastMessage={'placeholder'}
-						messageTimeStamp="placeholder"
-					/>
-				{/each}
 			{:else}
 				<ul role="list" class=" px-4 divide-slate-500">
 					<Friend
@@ -165,7 +213,6 @@
 				</ul>
 			{/if}
 		</div>
-	
 	</aside>
 {:else}
 	<Authentication />
@@ -219,14 +266,12 @@
 	<!--	</button>-->
 {/if}
 
-
 <style lang="postcss">
-    .toastEnter {
-        @apply transform ease-out duration-300 transition translate-y-0 opacity-100 sm:translate-x-0;
-    }
+	.toastEnter {
+		@apply transform ease-out duration-300 transition translate-y-0 opacity-100 sm:translate-x-0;
+	}
 
-    .toastLeave {
-        @apply transition ease-in duration-100 opacity-0;
-    }
-
+	.toastLeave {
+		@apply transition ease-in duration-100 opacity-0;
+	}
 </style>
